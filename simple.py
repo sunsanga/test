938a31bb99028b36fd46f14ad81d0e7b89197304
sunsanga <93355868+sunsanga@users.noreply.github.com>
2023-03-14 04:45:13 -0700

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,4 +1,23 @@
+#import sys
+#print ('Number of arguments:', len(sys.argv), 'arguments.')
+#print ('Argument List:', str(sys.argv))
+import getopt
 import sys
-
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
+ 
+first =""
+last =""
+argv = sys.argv[1:]
+try:
+    options, args = getopt.getopt(argv, "f:l:",
+                               ["first =",
+                                "last ="])
+except:
+    print("Error Message ")
+ 
+for name, value in options:
+    if name in ['-f', '--first']:
+        first = value
+    elif name in ['-l', '--last']:
+        last = value
+ 
+print(first + " " + last)
